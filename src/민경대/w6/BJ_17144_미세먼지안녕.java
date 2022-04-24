d8b5d25fce486aac8f0eb339d7a50efb963f8b00
민철 <@DESKTOP-DINA74T>
2022-04-24 15:08:09 +0900

    ll

--- src/민경대/w6/BJ_17144_미세먼지안녕.java
+++ src/민경대/w6/BJ_17144_미세먼지안녕.java
@@ -1,28 +1,25 @@
-package 민경대.w6;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/*
- * 백준 G4 ( https://www.acmicpc.net/problem/17144 )
- */
-public class BJ_17144_미세먼지안녕 {
+
+public class Main {
 
 	static int R, C, T, ans;
 	static int[][] map, tmp;
-	static int c1 = -1, c2 = -1; // 공기청정기 y좌표
+	static int c1 = -1, c2 = -1; 
 
-	static int[] dy = { -1, 1, 0, 0 }; // 상 하 좌 우
+	static int[] dy = { -1, 1, 0, 0 }; 
 	static int[] dx = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		R = Integer.parseInt(st.nextToken()); // 행
-		C = Integer.parseInt(st.nextToken()); // 열
-		T = Integer.parseInt(st.nextToken()); // 진행 시간
+		R = Integer.parseInt(st.nextToken()); 
+		C = Integer.parseInt(st.nextToken()); 
+		T = Integer.parseInt(st.nextToken()); 
 
 		map = new int[R][C];
 		for (int i = 0; i < R; i++) {
@@ -31,18 +28,18 @@ public class BJ_17144_미세먼지안녕 {
 				map[i][j] = Integer.parseInt(st.nextToken());
 				if (map[i][j] == -1) {
 					if (c1 == -1) {
-						c1 = i; // 위쪽 공기청정기 좌표
+						c1 = i; 
 					} else {
-						c2 = i; // 아래쪽 공기청정기 좌표
+						c2 = i; 
 					}
 				}
 			}
 		}
 
 		for (int i = 0; i < T; i++) {
-			spread(); // 확산
-			clean1(); // 위쪽 공기청정기
-			clean2(); // 아래쪽 공기청정기
+			spread(); 
+			clean1(); 
+			clean2(); 
 		}
 
 		for (int i = 0; i < R; i++) {
@@ -61,7 +58,7 @@ public class BJ_17144_미세먼지안녕 {
 
 		for (int y = 0; y < R; y++) {
 			for (int x = 0; x < C; x++) {
-				int cnt = 0; // 확산된 수
+				int cnt = 0; 
 				if (map[y][x] > 0) {
 					for (int d = 0; d < 4; d++) {
 						int ny = y + dy[d];
@@ -90,7 +87,7 @@ public class BJ_17144_미세먼지안녕 {
 		}
 	}
 
-	private static void clean1() { // 위쪽 공기청정기 반시계방향 순회
+	private static void clean1() { 
 		int sy = c1 - 1;
 		int sx = 0;
 
@@ -123,7 +120,7 @@ public class BJ_17144_미세먼지안녕 {
 
 	}
 
-	private static void clean2() { // 아래쪽 공기청정기 시계방향 순회
+	private static void clean2() { 
 		int sy = c2 + 1;
 		int sx = 0;
 
